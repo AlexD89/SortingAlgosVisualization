6b2c42e2356909aa6793ef74ce7d1641ab251df4
Alex D <alexander.dziuba@gmail.com>
2021-12-03 16:17:32 -0500

    Created swapper function to swap bars

--- src/index.js
+++ src/index.js
@@ -7,13 +7,36 @@ document.addEventListener("DOMContentLoaded", () => {
                 .attr("height","300px")
                 .attr("width","500px");
 
-    svg.selectAll("rect")
+    const bars = svg.selectAll("rect")
         .data(data)
         .enter().append("rect")
-            .attr("color","red")
-            .attr("height", function(d,i){return (d*10)})
+            .attr("class", function(d,i){return `bars bar-${i}`})
+            .attr("height", function(d,i){return (d*6)})
             .attr("width", function(){return 500/data.length - 1})
             .attr("x",function(d,i) {return (i * (500 / data.length))})
-            .attr("y", function(d, i ){return 300 - (d * 10)});
+            .attr("y", function(d, i ){return 300 - (d*6)});
 
+    const bar1 = svg.select(".bar-1");
+    const bar2 = svg.select(".bar-2");
+    const bar3 = svg.select(".bar-3");
+
+    function swapper(bar1,bar2){
+        bar1.transition()
+            .duration(2000)
+            .attr("x", `${bar2.attr("x")}`);
+
+        bar2.transition()
+            .duration(2000)
+            .attr("x", `${bar1.attr("x")}`);
+    }
+
+
+    for (let i = 2; i < data.length-1; i++){
+        let tempIdx svg.select;
+        currentBar = svg.select(`.bar-${i}`);
+        nextBar = svg.select(`.bar-${i+1}`);
+    }
+    swapper(bar1,bar3);
+
+  
 });
