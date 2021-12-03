7103075194ea2ff7b15e444d35bc36b212e9fecc
Alex D <alexander.dziuba@gmail.com>
2021-12-03 11:25:21 -0500

    Testing bar chart with D3

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const container = document.querySelector("div");
-    container.classList.add("grid");
+    const data = [4, 7, 2, 33, 22, 11, 19, 3, 6,
+                     7, 2, 33, 22, 11, 19, 3, 6,
+                     7, 2, 33, 22, 11, 19, 3, 6];
+
+    const svg = d3.select("#graph1").append("svg")
+                .attr("height","300px")
+                .attr("width","500px");
+
+    svg.selectAll("rect")
+        .data(data)
+        .enter().append("rect")
+            .attr("color","red")
+            .attr("height", function(d,i){return (d*10)})
+            .attr("width", function(){return 500/data.length - 1})
+            .attr("x",function(d,i) {return (i * (500 / data.length))})
+            .attr("y", function(d, i ){return 300 - (d * 10)});
+
 });
