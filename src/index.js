d0eaad0e6f4eb836653a7d4853f2a0a96cdc3728
Alex D <alexander.dziuba@gmail.com>
2021-12-03 23:43:39 -0500

    Working version of Generate control

--- src/index.js
+++ src/index.js
@@ -12,13 +12,41 @@ document.addEventListener("DOMContentLoaded", () => {
                 .attr("width", width)
                 .attr("style", "border: 1px solid black");
 
-    const graph2 = d3.select("#graph1").append("svg")
+    const graph2 = d3.select("#graph2").append("svg")
                 .attr("height", height)
                 .attr("width", width)
                 .attr("style", "border: 1px solid black");
 
     utils.renderGraph(graph1, data);
-    utils.renderGraph(graph2, data)
+    utils.renderGraph(graph2, data);
+
+    
+    // const sizeOfArr = document.getElementsByName("array-size");
+    // console.log(sizeOfArr);
+    const arrayForm = document.getElementById("controls-form");
+    arrayForm.addEventListener("submit", (e)=> {
+        e.preventDefault();
+        let arraySize = document.getElementById("array-size");
+        if(arraySize.value === "small") {
+            arraySize = 25;
+        } else if (arraySize.value === "medium") {
+            arraySize = 50;
+        } else if (arraySize.value === "large") {
+            arraySize = 100;
+        }
+
+        let arrayType = document.getElementById("array-type");
+        const data = utils.generateRandomArray(arraySize,30);
+        if (arrayType.value === "asc") data.sort((a,b) => (a-b));
+        if (arrayType.value === "desc") data.sort((a, b) => (a - b)).reverse();
+        console.log(data)
+
+        d3.selectAll("svg > *").remove();
+        utils.renderGraph(graph1,data);
+        utils.renderGraph(graph2,data);
+    })
+
+
 
     const bar1 = graph1.select(".bar-1");
     const bar2 = graph1.select(".bar-2");
