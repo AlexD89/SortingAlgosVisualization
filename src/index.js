baf126901ad2349fd37eecd1ea4b44f0506db4a1
Alex D <alexander.dziuba@gmail.com>
2021-12-04 19:53:08 -0500

    Working swapper function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import * as utils from "./scripts/utils.js";
 
+
 document.addEventListener("DOMContentLoaded", () => {
 
     const data = utils.generateRandomArray(25,45);
@@ -39,34 +40,53 @@ document.addEventListener("DOMContentLoaded", () => {
         const data = utils.generateRandomArray(arraySize,30);
         if (arrayType.value === "asc") data.sort((a,b) => (a-b));
         if (arrayType.value === "desc") data.sort((a, b) => (a - b)).reverse();
-        console.log(data)
 
         d3.selectAll("svg > *").remove();
         utils.renderGraph(graph1,data);
         utils.renderGraph(graph2,data);
     })
 
-
-
     const bar1 = graph1.select(".bar-1");
     const bar2 = graph1.select(".bar-2");
     const bar3 = graph1.select(".bar-3");
     const bar4 = graph1.select(".bar-4");
 
-    function swapper(bar1,bar2){
-        bar1.transition()
+
+    async function swapper(bar1,bar2){
+
+        const barOneTransition = bar1.transition()
             .delay(1000)
             .duration(2000)
             .attr("x", `${bar2.attr("x")}`)
+            .end();
             
-        bar2.transition()
+            
+        const barTwoTransition = bar2.transition()
             .delay(1000)
             .duration(2000)
             .attr("x", `${bar1.attr("x")}`)
-    }
-    
-    swapper(bar1,bar2);
-    swapper(bar3,bar4);
+            .end();
 
-            
+        const tempIdx = bar1.attr('class');
+        bar1.attr('class', bar2.attr('class'));
+        bar2.attr('class', tempIdx);
+
+        return Promise.all([barOneTransition, barTwoTransition]);
+    }
+
+    // swapper(bar1,bar2).then(() => swapper(bar1,bar2))
+    async function parse(){
+        for (let i = 0; i < data.length-1; i++){
+            const currentBar = graph1.select(`.bar-${i}`);
+            const nextBar = graph1.select(`.bar-${i+1}`);
+            console.log(currentBar);
+            console.log(nextBar);
+            await swapper(currentBar, nextBar);
+        }
+        // await swapper(bar1,bar2);
+        // swapper(bar3,bar4);
+    }
+
+    parse();
+
 });
