64fbcd9dd530de844aa3443f268d2562cccfdde5
Alex D <alexander.dziuba@gmail.com>
2021-12-09 10:33:16 -0500

    url fix

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ const config = {
                     MiniCssExtractPlugin.loader, // create bundled css file
                     {
                         loader: 'css-loader', // resolves @import statements
-                        options: { url: false } // don't resolve url() statements
+                        options: { url: true } // don't resolve url() statements
                     },
                     'sass-loader', // compiles sass to css
                 ]
